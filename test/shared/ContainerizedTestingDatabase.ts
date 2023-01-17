721a42b6a0bc4f9cc5a6d016abd4e0aa9238f719
Alexander Kampf <alexander.kampf@thenativeweb.io>
2023-01-17 12:02:22 +0100

    chore: Start with testing database

--- test/shared/ContainerizedTestingDatabase.ts
+++ test/shared/ContainerizedTestingDatabase.ts
@@ -1,5 +1,28 @@
+import { Container } from './docker/Container';
+import { Image } from './docker/Image';
+
 class ContainerizedTestingDatabase {
-  constructor () {}
+  private readonly command: string;
+
+  private container?: Container;
+
+  private readonly image: Image;
+
+  private readonly options: any;
+
+  private readonly isFirstRun = true;
+
+  constructor (image: Image, command: string, options: any) {
+    this.command = command;
+    this.image = image;
+    this.options = options;
+  }
+
+  private start () {
+    this.container = this.image.run(this.command, true, true);
+    const exposedPort = this.container.getExposedPort(3_000);
+    const baseUrl = `http://localhost:${exposedPort}`;
+  }
 }
 
 export { ContainerizedTestingDatabase };
