8dca9c157ecb68c4cbab96356764838c0bf171c6
Noah Hummel <noah.hummel@thenativeweb.io>
2023-01-19 14:18:08 +0100

    chore: remove unneeded import

--- lib/handlers/observeEvents/observeEvents.ts
+++ lib/handlers/observeEvents/observeEvents.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { StatusCodes } from 'http-status-codes';
 import { Readable } from 'stream';
 import { Client } from '../../Client';
