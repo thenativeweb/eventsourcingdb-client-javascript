0cfec15323b7d54b3b78cd7f1b5cb86fa55fceaf
Noah Hummel <strangedev@posteo.net>
2023-01-19 15:38:14 +0100

    feat: Add retrying and aborting to observe-events (#7)
    * chore: add retryWithBackoff
    
    * feat: add retries and cancellation
    
    * chore: remove unneeded import
    
    * Update lib/util/error/CancelationError.ts
    
    Co-authored-by: Golo Roden <golo.roden@thenativeweb.io>
    
    * Update lib/util/retry/RetryError.ts
    
    Co-authored-by: Golo Roden <golo.roden@thenativeweb.io>
    
    * fix: use #private
    
    * fix: spelling
    
    Co-authored-by: Golo Roden <golo.roden@thenativeweb.io>

--- lib/util/ndjson/LinesDecoder.ts
+++ lib/util/ndjson/LinesDecoder.ts
@@ -1,33 +1,33 @@
 import { StringDecoder } from 'string_decoder';
 
 class LinesDecoder {
-	private textBuffer: string;
-	private decoder: StringDecoder;
+	#textBuffer: string;
+	#decoder: StringDecoder;
 
-	constructor(encoding?: BufferEncoding) {
-		this.textBuffer = '';
-		this.decoder = new StringDecoder(encoding);
+	public constructor(encoding?: BufferEncoding) {
+		this.#textBuffer = '';
+		this.#decoder = new StringDecoder(encoding);
 	}
 
-	write(chunk: Buffer): string[] {
-		this.textBuffer += this.decoder.write(chunk);
+	public write(chunk: Buffer): string[] {
+		this.#textBuffer += this.#decoder.write(chunk);
 
 		const lines: string[] = [];
 
 		let lineStart = 0;
-		for (let charIndex = 0; charIndex < this.textBuffer.length; charIndex++) {
-			const char = this.textBuffer[charIndex];
+		for (let charIndex = 0; charIndex < this.#textBuffer.length; charIndex++) {
+			const char = this.#textBuffer[charIndex];
 
 			if (char !== '\n') {
 				continue;
 			}
 
-			const line = this.textBuffer.slice(lineStart, charIndex);
+			const line = this.#textBuffer.slice(lineStart, charIndex);
 			lineStart = charIndex + 1;
 
 			lines.push(line);
 		}
-		this.textBuffer = this.textBuffer.slice(lineStart);
+		this.#textBuffer = this.#textBuffer.slice(lineStart);
 
 		return lines;
 	}
