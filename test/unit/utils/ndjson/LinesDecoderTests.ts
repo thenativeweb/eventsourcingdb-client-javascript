978c1f3f82cbae51e7adde025a91ffee95fcf0e4
Noah Hummel <noah.hummel@thenativeweb.io>
2023-01-24 12:22:48 +0100

    chore: test LinesDecoder

---
+++ test/unit/utils/ndjson/LinesDecoderTests.ts
@@ -0,0 +1,23 @@
+import { assert } from 'assertthat';
+import { LinesDecoder } from '../../../../lib/util/ndjson/LinesDecoder';
+
+suite('LinesDecoder', (): void => {
+	test('returns all completed lines on write().', async (): Promise<void> => {
+		const decoder = new LinesDecoder();
+
+		const actualLines = decoder.write(Buffer.from('hello\nworld\nfoobar'));
+
+		assert.that(actualLines).is.equalTo(['hello', 'world']);
+	});
+	test('buffers incomplete lines and returns them on write() as soon as they are completed.', async (): Promise<void> => {
+		const decoder = new LinesDecoder();
+
+		let actualLines = decoder.write(Buffer.from('incomplete'));
+
+		assert.that(actualLines).is.equalTo([]);
+
+		actualLines = decoder.write(Buffer.from(' line\n'));
+
+		assert.that(actualLines).is.equalTo(['incomplete line']);
+	});
+});
