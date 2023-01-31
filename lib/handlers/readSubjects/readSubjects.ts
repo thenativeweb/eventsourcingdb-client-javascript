ef201c7c68db3ede7e2f968044bd52811a317def
Alexander Kampf <alexander.kampf@thenativeweb.io>
2023-01-31 11:22:28 +0100

    Improve error handling in readSubjects

--- lib/handlers/readSubjects/readSubjects.ts
+++ lib/handlers/readSubjects/readSubjects.ts
@@ -6,15 +6,39 @@ import { readNdJsonStream } from '../../util/ndjson/readNdJsonStream';
 import { isStreamError } from '../isStreamError';
 import { isSubject } from './isSubject';
 import { CancelationError } from '../../util/error/CancelationError';
+import { ValidationError } from '../../util/error/ValidationError';
+import { InvalidParameterError } from '../../util/error/InvalidParameterError';
+import { CustomError } from '../../util/error/CustomError';
+import { InternalError } from '../../util/error/InternalError';
+import { ServerError } from '../../util/error/ServerError';
 
 const readSubjects = async function* (
 	client: Client,
 	abortController: AbortController,
 	options: ReadSubjectsOptions,
 ): AsyncGenerator<string, void, void> {
-	validateReadSubjectsOptions(options);
+	await wrapError(
+		() => {
+			validateReadSubjectsOptions(options);
+		},
+		(ex) => {
+			if (ex instanceof ValidationError) {
+				throw new InvalidParameterError('options', ex.message);
+			}
+		},
+	);
 
-	const requestBody = JSON.stringify(options);
+	const requestBody = await wrapError(
+		() => {
+			return JSON.stringify(options);
+		},
+		(ex) => {
+			throw new InvalidParameterError(
+				'options',
+				'Parameter contains values that cannot be marshaled.',
+			);
+		},
+	);
 
 	const response = await wrapError(
 		async () =>
@@ -25,11 +49,11 @@ const readSubjects = async function* (
 				abortController,
 			}),
 		async (error) => {
-			if (error instanceof CancelationError) {
-				return error;
+			if (error instanceof CustomError) {
+				throw error;
 			}
 
-			return new ChainedError('Failed to read subjects.', error);
+			throw new InternalError(error);
 		},
 	);
 
@@ -45,7 +69,7 @@ const readSubjects = async function* (
 			continue;
 		}
 
-		throw new Error(
+		throw new ServerError(
 			`Failed to read subjects, an unexpected stream item was received: '${message}'.`,
 		);
 	}
