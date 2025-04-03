59b7f1e9dff9aea65cbc95e1eb2b0a2680a674bc
Golo Roden <golo.roden@thenativeweb.io>
2025-04-03 19:39:02 +0200

    chore: Remove pseudo-sample code.

--- src/index.ts
+++ src/index.ts
@@ -3,43 +3,5 @@ import type { ObserveEventsOptions } from './ObserveEventsOptions.js';
 import { isSubjectOnEventId, isSubjectPristine } from './Precondition.js';
 import type { ReadEventsOptions } from './ReadEventsOptions.js';
 
-const bla = async () => {
-	const client = new Client(new URL('http://localhost:3000'), 'secret');
-
-	await client.ping();
-	console.log('Ping successful');
-
-	const writtenEvents = await client.writeEvents(
-		[
-			{
-				source: 'https://www.irgendwas.de',
-				subject: '/bla/234',
-				type: 'com.example.bla',
-				data: {
-					foo: 'bar',
-					baz: 123,
-					qux: true,
-				},
-			},
-		],
-		[isSubjectPristine('/bla/234')],
-	);
-	console.log('Written events:', writtenEvents);
-
-	// const events = client.readEvents(
-	// 	'/',
-	// 	{
-	// 		recursive: true,
-	// 	},
-	// 	new AbortController(),
-	// );
-
-	// for await (const event of events) {
-	// 	console.log('Event read:', event);
-	// }
-};
-
-bla();
-
 export { Client, isSubjectPristine, isSubjectOnEventId };
 export type { ReadEventsOptions, ObserveEventsOptions };
