a5a30e7c5a22b68eea706afaa83b0002741269bc
Marc Jegliewski <marc.jegliewski@consultit.online>
2025-09-21 15:47:23 +0200

    Export isEventQlQueryTrue

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ import type { EventCandidate } from './EventCandidate.js';
 import type { EventType } from './EventType.js';
 import { isSubjectOnEventId } from './isSubjectOnEventId.js';
 import { isSubjectPristine } from './isSubjectPristine.js';
+import { isEventQlQueryTrue } from './isEventQlQueryTrue.js';
 import type { ObserveEventsOptions } from './ObserveEventsOptions.js';
 import type { Precondition } from './Precondition.js';
 import type { ReadEventsOptions } from './ReadEventsOptions.js';
 
-export { Client, Container, isSubjectOnEventId, isSubjectPristine };
+export { Client, Container, isSubjectOnEventId, isSubjectPristine, isEventQlQueryTrue };
 export type {
 	Event,
 	EventCandidate,
