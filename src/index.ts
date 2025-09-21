254bc4e39dd23e68fd73ae7de49fc27cc4b9cc06
Marc Jegliewski <marc.jegliewski@gmail.com>
2025-09-21 16:14:08 +0200

    fix: Missing export of isEventQlQueryTrue (#270)
    * Export isEventQlQueryTrue
    
    * Fix import and export to be in alphabetical order

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import { Container } from './Container.js';
 import type { Event } from './Event.js';
 import type { EventCandidate } from './EventCandidate.js';
 import type { EventType } from './EventType.js';
+import { isEventQlQueryTrue } from './isEventQlQueryTrue.js';
 import { isSubjectOnEventId } from './isSubjectOnEventId.js';
 import { isSubjectPristine } from './isSubjectPristine.js';
 import type { ObserveEventsOptions } from './ObserveEventsOptions.js';
 import type { Precondition } from './Precondition.js';
 import type { ReadEventsOptions } from './ReadEventsOptions.js';
 
-export { Client, Container, isSubjectOnEventId, isSubjectPristine };
+export { Client, Container, isEventQlQueryTrue, isSubjectOnEventId, isSubjectPristine };
 export type {
 	Event,
 	EventCandidate,
